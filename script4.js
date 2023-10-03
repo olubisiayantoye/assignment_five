fa414bc16e1ff17e64d20db58d230f53945cc2e8
Olubisi O Ayantoye <bisikingson@gmail.com>
2023-10-03 11:43:28 +0100

    Add files via upload

---
+++ script4.js
@@ -0,0 +1,19 @@
+
+
+
+(function () {
+
+var names = ["James", "John", "Janet", "Jason", "Paul", "Trump", "Larry", "Paula", "Laura", "Jim"];
+
+for (var i = 0; i < names.length; i++) {
+  var firstLetter = names[i].charAt(0).toLowerCase();
+
+  if (firstLetter === 'j') {
+    byeSpeaker(names[i]);
+  }
+  else {
+    helloSpeaker(names[i]);
+  }
+}
+
+})();
